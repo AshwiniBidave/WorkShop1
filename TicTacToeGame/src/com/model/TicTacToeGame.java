76421f1205e583226cdeb1b4fd9ee9380dada2bb
AshwiniBidave <ashwinibidave1993@gmail.com>
2022-10-17 20:35:33 +0530

    ADD:Adding EmptySpace

--- TicTacToeGame/src/com/model/TicTacToeGame.java
+++ TicTacToeGame/src/com/model/TicTacToeGame.java
@@ -2,9 +2,30 @@ package com.model;
 
 public class TicTacToeGame {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public void createTicTacGame() {
+		char[] board= new char[10];
+		int start=1;
+		//for(int start =1;start<=board.length;start++)
+		while(start<board.length)
+		{
+		
+	     board[start]=' ';
+		start++;
+			
+		}
+	
 	}
+			
+		
 
+
+		public static void main(String[] args) 
+		{
+
+		TicTacToeGame create=new TicTacToeGame();
+	   create.createTicTacGame();
+		
+		
+
+	    }
 }
