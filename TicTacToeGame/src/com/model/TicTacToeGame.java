19bbb31afbf4e66011d63ec3de858c3f7792adb8
AshwiniBidave <ashwinibidave1993@gmail.com>
2022-10-18 00:22:40 +0530

    ADD:Adding code for choose player

--- TicTacToeGame/src/com/model/TicTacToeGame.java
+++ TicTacToeGame/src/com/model/TicTacToeGame.java
@@ -1,31 +1,45 @@
 package com.model;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
+	char computer, player;
 
 	public void createTicTacGame() {
-		char[] board= new char[10];
-		int start=1;
-		//for(int start =1;start<=board.length;start++)
-		while(start<board.length)
-		{
-		
-	     board[start]=' ';
-		start++;
-			
+		char[] board = new char[10];
+		int start = 1;
+		// for(int start =1;start<=board.length;start++)
+		while (start < board.length) {
+
+			board[start] = ' ';
+			start++;
+
 		}
-	
+
 	}
-			
-		
 
+	public void choosingXorO() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter The Input");
+		char player = sc.next().charAt(0);
+		sc.close();
+		if (player == 'X') {
+			computer = 'O';
 
-		public static void main(String[] args) 
-		{
+		} else if (player == 'O') {
+			computer = 'X';
 
-		TicTacToeGame create=new TicTacToeGame();
-	   create.createTicTacGame();
-		
-		
+		} else {
+			System.out.println("input is invalid");
+		}
 
-	    }
+	}
+
+	public static void main(String[] args) {
+
+		TicTacToeGame create = new TicTacToeGame();
+		create.createTicTacGame();
+		create.choosingXorO();
+
+	}
 }
